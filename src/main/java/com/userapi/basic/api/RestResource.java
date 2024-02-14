34cd1eedb552564fd4c107f541b52357dee6f68d
Shreeja90 <147870884+Shreeja90@users.noreply.github.com>
2024-02-14 14:32:34 -0500

    updated the post request and added delete request

--- src/main/java/com/userapi/basic/api/RestResource.java
+++ src/main/java/com/userapi/basic/api/RestResource.java
@@ -1,5 +1,7 @@
 package com.userapi.basic.api;
 
+
+
 import static com.userapi.basic.api.Route.BASE_PATH;
 import static io.restassured.RestAssured.given;
 
@@ -14,36 +16,59 @@ import io.restassured.specification.ResponseSpecification;
 
 public class RestResource {
 	
-    public static Response post(String path, Object requestUserApi){
+    public static Response post(String path, Object requestUserApi, int sCode){
         return given(getRequestSpec()).
                 body(requestUserApi).
                 auth().basic("Numpy@gmail.com","tim123").
                 when().post(path).
                 then().spec(getResponseSpec()).
-                statusCode(201).
+                statusCode(sCode).
                 extract().response();
     }
     
-    public static Response get(String path){
+    public static Response get(String path, int sCode){
         return given(getRequestSpec()).
         		auth().basic("Numpy@gmail.com","tim123").
         		when().get(path).
         		then().spec(getResponseSpec()).
+        		statusCode(sCode).
                 extract().
                 response();
     }
+    
+    public static Response update(String path, Object requestUserApi,int sCode ){
+        return given(getRequestSpec()).
+        		auth().basic("Numpy@gmail.com","tim123").
+                body(requestUserApi).
+                when().put(path).
+                then().spec(getResponseSpec()).
+                statusCode(sCode).
+                extract().
+                response();
+    }
+    
+    public static Response delete(String path, int sCode){
+        return given(getRequestSpec()).
+        		auth().basic("Numpy@gmail.com","tim123").
+        		when().delete(path).
+        		then().spec(getResponseSpec()).
+        		statusCode(sCode).
+                extract().
+                response();
+    }
+    
 
     public static RequestSpecification getRequestSpec(){
         return new RequestSpecBuilder().
                 //setBaseUri(System.getProperty("BASE_URI")).
         		setBaseUri("https://userapi-8877aadaae71.herokuapp.com").
                 setBasePath(BASE_PATH).
-                setContentType(ContentType.JSON).
-                addFilter(new AllureRestAssured()).
-                log(LogDetail.ALL).
-                build();
+                setContentType(ContentType.JSON). //this line is used to set the content type to json
+                addFilter(new AllureRestAssured()).//this line is to generate allure results
+                log(LogDetail.ALL). //logger
+                build(); //this method is from rest assured used to build the header for the request
     }
-    
+    //this method is written to log and build the response
     public static ResponseSpecification getResponseSpec(){
         return new ResponseSpecBuilder().
                 log(LogDetail.ALL).
--- src/main/java/com/userapi/tests/Test01.java
+++ src/main/java/com/userapi/tests/Test01.java
@@ -2,12 +2,16 @@ package com.userapi.tests;
 
 import static com.userapi.basic.api.Route.USERS;
 import static com.userapi.basic.api.Route.NEWUSERS;
+import static com.userapi.basic.api.Route.DELETEUSER;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.json.JSONObject;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.io.File;
 
 import com.userapi.basic.api.RestResource;
 
@@ -19,11 +23,24 @@ public class Test01 {
         System.out.println("STARTING TEST: " + m.getName());
     }
     
-    @Test
-    public void getRequest() {
-    	RestResource.get(USERS);
-    	File jsonData = new File("/Users/shreeeja/eclipse-workspace/RestAssured_Testng/src/main/java/com/userapi/tests/user.json");
-    	RestResource.post(NEWUSERS,jsonData);
+    @Test(groups = {"users", "smoke"})	
+    public void getRequest() throws IOException {
+    	RestResource.get(USERS, 200);
+    }
+    
+    @Test(groups = {"create", "smoke"})	
+    public void postCreateUser() throws IOException
+    {
+    	String content = new String(Files.readAllBytes(Paths.get("/Users/shreeeja/eclipse-workspace/UserAPI/src/main/java/com/userapi/tests/user.json")));
+        // Parse JSON string into JSONObject
+        JSONObject jsonObject = new JSONObject(content);
+    	RestResource.post(NEWUSERS,jsonObject.toString(), 201);
+    }
+    
+    @Test(groups = {"delete"})	
+    public void deleteuser() {
+    	RestResource.delete(DELETEUSER +"/7612", 200);
+    	
     }
 
 }
